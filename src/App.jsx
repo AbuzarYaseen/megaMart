c0db8be1950b88f733f3872b90d7df71ac8ae25e
Abuzar Yaseen <abuzaryaseen88@gmail.com>
2024-01-25 17:50:51 +0500

    routes protected

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Home from "./Components/Home/Home";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Protected from "./pages/protectedRoutes/Protected";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Protected, {
+  ProtectedBooks,
+  ProtectedGadets,
+  ProtectedMobiles,
+  ProtectedDeals,
+  ProtectedToys,
+} from "./pages/protectedRoutes/Protected";
 import Gadets from "./pages/gadets/Gadets";
 import Books from "./pages/books/Books";
 import Mobiles from "./pages/mobiles/Mobiles";
@@ -22,13 +28,12 @@ const App = () => {
           <Route path="/" element={<SignIn />} />
           <Route path="signin" element={<SignIn />} />
           <Route path="signup" element={<SignUp />} />
-          
-          <Route path="home" element={<Protected Component = {Home}/>}/>
-          <Route path="gadets" element={<Gadets/>}/>
-          <Route path="books" element={<Books/>}/>
-          <Route path="mobiles" element={<Mobiles/>}/>
-          <Route path="toys" element={<Toys/>}/>
-          <Route path="deals" element={<Deals/>}/>
+          <Route path="home" element={<Protected Component={Home} />} />
+          <Route path="gadets" element={<ProtectedGadets />} />
+          <Route path="books" element={<ProtectedBooks />} />
+          <Route path="mobiles" element={<ProtectedMobiles />} />
+          <Route path="toys" element={<ProtectedToys />} />
+          <Route path="deals" element={<ProtectedDeals />} />
         </Routes>
       </Router>
       <ToastContainer />
--- src/pages/protectedRoutes/Protected.jsx
+++ src/pages/protectedRoutes/Protected.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Deals from "../deals/Deals";
+import Books from "../books/Books";
+import Mobiles from "../mobiles/Mobiles";
+import Toys from "../toys/Toys";
+import Gadets from "../gadets/Gadets";
 
 const Protected = ({ Component }) => {
   const navigate = useNavigate();
@@ -14,6 +19,34 @@ const Protected = ({ Component }) => {
       <Component />
     </>
   );
+};
+
+const ProtectedGadets = () => {
+  return <Protected Component={Gadets} />;
+};
+
+const ProtectedBooks = () => {
+  return <Protected Component={Books} />;
+};
+
+const ProtectedMobiles = () => {
+  return <Protected Component={Mobiles} />;
+};
+
+const ProtectedDeals = () => {
+  return <Protected Component={Deals} />;
+};
+
+const ProtectedToys = () => {
+  return <Protected Component={Toys} />;
+};
+
+export {
+  ProtectedGadets,
+  ProtectedBooks,
+  ProtectedMobiles,
+  ProtectedDeals,
+  ProtectedToys,
 };
 
 export default Protected;
